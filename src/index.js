617f70fb096165bd5229084484b0e18edfa83be3
P-S-S-7 <psinghskekhawat982@gmail.com>
2024-07-17 17:50:40 +0530

    Custom api response and error handling - utils

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 import connectDB from "./db/index.js";
+import dotenv from "dotenv";
+import {app} from "./app.js";
 
-connectDB();
+dotenv.config({ path: "./env" });
+
+connectDB()
+.then (() => {
+  app.listen(process.env.PORT || 8000, () => {
+    console.log(`Server is running at port:${process.env.PORT}`);
+  })
+})
+.catch((err) => {
+    console.error("MongoDB connection error: ", err);
+})
+
 
 /*
 import express from "express";
---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,11 @@
+const ApiResponse = {
+    constructor(statusCode, data, message = "Succcess") {
+        this.statusCode = statusCode;
+        this.data = data;
+        this.message = message;
+        this.success = statusCode < 400;
+    }
+}
+
+export { ApiResponse };
+    
